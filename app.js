d9169fcda1f0277ec5bc07daf1777d206e8654f0
Ishaan-11 <ishaansharma1998@gmail.com>
2020-09-03 18:52:28 +0530

    POST a New Article

--- app.js
+++ app.js
@@ -25,9 +25,22 @@ app.get("/articles", function(req, res) {
         } else {
             res.send(err);
         }
-    })
-})
+    });
+});
 
+app.post("/articles", function(req, res) {
+    const newArticle = new Article({
+        title: req.body.title,
+        content: req.body.content
+    });
+    newArticle.save(function(err) {
+        if(!err) {
+            res.send("Successfully added a new article.");
+        } else {
+            res.send(err);
+        }
+    });
+});
 
 app.listen(3000, function() {
     console.log("Server started on port 3000");
