b4348a5c6b6a4d287f252658130e22cf0fdbf942
Ishaan-11 <ishaansharma1998@gmail.com>
2020-09-03 21:21:22 +0530

    PUT a Specific Article

--- app.js
+++ app.js
@@ -64,6 +64,18 @@ app.route("/articles/:articleTitle")
             res.send("No articles matching that title was found");
         }
     });
+})
+.put(function(req, res) {
+    Article.update(
+        {title: req.params.articleTitle},
+        {title: req.body.title, content: req.body.content},
+        {overwrite: true},
+        function(err) {
+            if(!err) {
+                res.send("Successfully updated articles");
+            }
+        }
+    );
 });
 
 app.listen(3000, function() {
